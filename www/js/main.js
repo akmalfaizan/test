6d99c3e7157a77cb0d046bb570196dddac3f89e2
akmal <akmal10@gmail.com>
2015-03-12 17:53:04 +0530

    asd

--- www/js/main.js
+++ www/js/main.js
@@ -11,6 +11,32 @@ function onDeviceReady() {
 	pictureSource = navigator.camera.PictureSourceType;
 	destinationType = navigator.camera.DestinationType;
 }
+//datepicker
+ function handleDates(elm, options) {
+        event.stopPropagation();
+        var currentField = $(elm);
+        var opts = options || {};
+        var minVal = opts.min || 0;
+        var maxVal = opts.max || 0;
+
+        var myNewDate = Date.parse(currentField.val()) || new Date();
+        if(typeof myNewDate === "number") {
+            myNewDate = new Date (myNewDate);
+        }
+
+        window.plugins.datePicker.show({
+            date : myNewDate,
+            mode : 'date',
+            minDate: Date.parse(minVal),
+            maxDate: Date.parse(maxVal)
+        }, function(returnDate) {
+            if(returnDate !== "") {
+                var newDate = new Date(returnDate);
+                currentField.val(getFormattedDate(newDate));
+            }
+            currentField.blur();
+        });
+    }
 
 //Capture Photo
 function takePhoto() {
