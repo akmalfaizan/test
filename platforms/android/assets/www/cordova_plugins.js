106315ba066a5bd28db18a428a31ce9b426c8640
akmal <akmal10@gmail.com>
2015-03-13 11:19:23 +0530

    paul datepicker

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/nl.moreapps.datepicker/www/datepicker.js",
+        "id": "nl.moreapps.datepicker.Datepicker",
+        "clobbers": [
+            "window.datePicker"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "nl.moreapps.datepicker": "1.0.2"
+}
 // BOTTOM OF METADATA
 });
